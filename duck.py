63d0acd2408ff92857e4a031bdf12e01baf2ca22
Nadeem Anwar <ibnul.aftab@proton.me>
2025-01-30 01:37:26 +0500

    corrected the top row
    now the time and text row will not repeat itself

--- duck.py
+++ duck.py
@@ -4,11 +4,13 @@ from os import path
 
 filePath = "log.csv"
 
-with open(filePath, 'a') as log:
+with open(filePath, "a") as log:
     csvLog = csv.writer(log)
-    readLog = open(filePath, "r")
+    with open(filePath, "r") as readLog:
+        if readLog.readline() == "":
+            csvLog.writerow(["time", "text"])
     while (True):
         userInput = input("You: ")
         if userInput == ":q":
             break
-        csvlog.writerow([datetime.now().strftime("%d-%m-%Y %H:%M:%S"), userInput])
+        csvLog.writerow([datetime.now().strftime("%d-%m-%Y %H:%M:%S"), userInput])
