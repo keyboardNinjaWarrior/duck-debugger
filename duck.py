280c868c4c9f6df1a5ce9a22d3bf121ce3812390
Nadeem Anwar <ibnul.aftab@proton.me>
2025-01-30 00:55:27 +0500

    comleted implement user input

--- duck.py
+++ duck.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 import csv
 
-# print(datetime.now())
-
 with open("log.csv", 'a') as log:
     csvlog = csv.writer(log)
     csvlog.writerow(["time", "text"])
+    while (True):
+        user_input = input("You: ")
+        if user_input == ":exit":
+            break
+        csvlog.writerow([datetime.now().strftime("%d-%m-%Y %H:%M:%S"), user_input])
