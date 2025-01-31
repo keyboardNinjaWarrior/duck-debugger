cf70a2ff75dfcf6d83633db66db6586d93aa057e
Nadeem Anwar <ibnul.aftab@proton.me>
2025-01-31 14:47:22 +0500

    added duck's picture

--- duck.py
+++ duck.py
@@ -1,6 +1,15 @@
 from datetime import datetime
 import csv
 from os import path
+from random import randint
+from shutil import get_terminal_size
+
+COL = get_terminal_size().columns
+
+with open("duck.txt", "r") as duck:
+    for line in duck:
+        catPixelRow = line.strip()
+        print(catPixelRow.center(COL))
 
 filePath = "log.csv"
 
@@ -14,3 +23,4 @@ with open(filePath, "a") as log:
         if userInput == ":q":
             break
         csvLog.writerow([datetime.now().strftime("%d-%m-%Y %H:%M:%S"), userInput])
+        print("ddb:", "quack " * randint(1, 3))
